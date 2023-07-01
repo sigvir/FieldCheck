7774fa8558f4ce405176e6c85350ee4aab91126b
seegah <seegah@gmail.com>
2023-07-01 08:35:56 -0700

    htlm template: Parking check

--- src/test/java/Template.java
+++ src/test/java/Template.java
@@ -13,67 +13,102 @@ public class Template {
     public static WebDriver driver;
 
     @Test
-    public void getPage() throws InterruptedException {
+    public void FillSearch() throws InterruptedException {
         driver.get("http://127.0.0.1:5500/index.html");
         driver.findElement(By.id("reset")).click(); // reset button
         driver.findElement(By.id("search")).sendKeys("Plaukimas baidaremis");
-        Thread.sleep(200);
+        driver.findElement(By.xpath("//*[@id=\"form\"]/button")).click();
+        String searchResult = driver.findElement(By.id("search-result")).getText();
+        System.out.println(searchResult.equals("Plaukimas baidaremis") ? "search pass" : "search fail");
+        Thread.sleep(200);//veikia
+    }
 
-        //***data entry**///
-
+    @Test
+    public void fillName() throws InterruptedException {
+        driver.get("http://127.0.0.1:5500/index.html");
         driver.findElement(By.id("name")).sendKeys("Vardenis");
-        Thread.sleep(200);
+        driver.findElement(By.xpath("//*[@id=\"form\"]/button")).click();
+        String nameResult = driver.findElement(By.id("name-result")).getText();
+        System.out.println(nameResult.equals("Vardenis") ? "name pass" : "name fail");
+        Thread.sleep(200);//veikia
+    }
+
+    @Test
+    public void fillPasword() throws InterruptedException {
+        driver.get("http://127.0.0.1:5500/index.html");
         driver.findElement(By.id("password")).sendKeys("Pavardenis");
+        driver.findElement(By.xpath("//*[@id=\"form\"]/button")).click();
+        String passwordResult = driver.findElement(By.id("password-result")).getText();
+        System.out.println(passwordResult.equals("Pavardenis") ? "password pass" : "password fail");
         Thread.sleep(200);
+    }
+
+    @Test
+    public void fillEmail() throws InterruptedException {
+        driver.get("http://127.0.0.1:5500/index.html");
         driver.findElement(By.id("email")).sendKeys("vardenis.pavardenis@gmail.com");
-        Thread.sleep(200);
+        driver.findElement(By.xpath("//*[@id=\"form\"]/button")).click();
+        String emailResult = driver.findElement(By.id("email-result")).getText();
+        System.out.println(emailResult.equals("vardenis.pavardenis@gmail.com") ? "email pass" : "email fail");
+        Thread.sleep(200);//veikia
+    }
+
+    @Test
+    public void fillAge() throws InterruptedException {
+        driver.get("http://127.0.0.1:5500/index.html");
         driver.findElement(By.id("age")).sendKeys("25");
-        Thread.sleep(200);
+        driver.findElement(By.xpath("//*[@id=\"form\"]/button")).click();
+        String ageResult = driver.findElement(By.id("age-result")).getText();
+        System.out.println(ageResult.equals("25") ? "age pass" : "age fail");
+        Thread.sleep(200);//veikia
+    }
 
-        //***box check***///
-
+    @Test
+    public void checkBoxes() throws InterruptedException {
+        driver.get("http://127.0.0.1:5500/index.html");
         driver.findElement(By.id("sleepInBed")).click(); // kur miegosim varnele
         driver.findElement(By.id("HaveMeal")).click();//ar valgysim varnele
         driver.findElement(By.id("mid")).click(); //plaukimo viduryje
-        Thread.sleep(200);
-        driver.findElement(By.xpath("/html/body/form/select/option[3]")).click();// click option 3
-        // Thread.sleep(200);
-        driver.findElement(By.xpath("/html/body/form/button")).click(); // press button
-
-        //***data check ***///
-
-        String nameResult = driver.findElement(By.id("name-result")).getText();
-        System.out.println(nameResult.equals("Vardenis") ? "name pass" : "name fail");
-
-        String ageResult = driver.findElement(By.id("age-result")).getText();
-        System.out.println(ageResult.equals("25") ? "age pass" : "age fail");
-
-        String searchResult = driver.findElement(By.id("search-result")).getText();
-        System.out.println(searchResult.equals("Plaukimas baidaremis") ? "search pass" : "search fail");
-
-        String passwordResult = driver.findElement(By.id("password-result")).getText();
-        System.out.println(passwordResult.equals("Plaukimas baidaremis") ? "password pass" : "password fail");
-
-        String emailResult = driver.findElement(By.id("email-result")).getText();
-        System.out.println(emailResult.equals("vardenis.pavardenis@gmail.com") ? "email pass" : "email fail");
-
+        driver.findElement(By.xpath("//*[@id=\"form\"]/button")).click();//push touch me
         String sleepInBedResult = driver.findElement(By.id("sleepInBed-result")).getText();
         System.out.println(sleepInBedResult.equals("Yes") ? "sleepInBed pass" : "sleepInBed fail");
-
         String HaveMealResult = driver.findElement(By.id("HaveMeal-result")).getText();
         System.out.println(HaveMealResult.equals("Yes") ? "HaveMeal pass" : "HaveMeal fail");
+        driver.findElement(By.xpath("/html/body/form/select/option[3]")).click();// click option triple
+        driver.findElement(By.xpath("//*[@id=\"form\"]/button")).click(); //push touch me
 
-        // if(text.equals("jonas")){
+        //***Parking check***///
+        driver.findElement(By.id("parking-result-start")).getText();
+        String parkingResultStart = driver.findElement(By.id("parking-result-start")).getText();
+        System.out.println(parkingResultStart.equals("Yes") ? "parkingResultStart pass" : "parkingResultStart fail");
+        Thread.sleep(100);
+
+        String parkingResultMid = driver.findElement(By.id("parking-result-mid")).getText();
+        System.out.println(parkingResultMid.equals("Yes") ? "parkingResultMid pass" : "parkingResultMid fail");
+        Thread.sleep(100);
+
+        String parkingResultEnd = driver.findElement(By.id("parking-result-end")).getText();
+        System.out.println(parkingResultEnd.equals("Yes") ? "parkingResultEnd pass" : "parkingResultEnd fail");
+        Thread.sleep(100);
+
+        Assert.assertEquals(driver.findElement(By.id("seats-result")).getText(), 1); //patikrina
+
+    }
+
+    @Ignore
+    @Test
+    public void parkingAnswer() throws InterruptedException {
+
+
+    }
+    // if(text.equals("jonas")){
 // System.out.println("zodis buvo jonas");
 // } else if (text.equals("labas")) {
 // System.out.println("zodis buvo labas");
 // }else {
 // System.out.println("zodis mums nezinomas");
 
-
-        // Assert.assertEquals(driver.findElement(By.id("name-result")).getText(),"Vardenis");
-
-    }
+    // Assert.assertEquals(driver.findElement(By.id("name-result")).getText(),"Vardenis");
 
     // @BeforeMethod
     //  public void beforeMethod() {
@@ -82,7 +117,7 @@ public class Template {
     public void beforeClass() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @AfterClass
@@ -90,5 +125,5 @@ public class Template {
         //driver.quit();
 
     }
-}
 
+}
